20c67602ec965feeb34c51e99527bf8f05d87415
jsgaston <69969971+jsgaston@users.noreply.github.com>
2025-06-23 23:55:02 +0200

    Update generate_song_list.py

--- generate_song_list.py
+++ generate_song_list.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     songs = get_audio_files_in_root()
     with open("song_list004.json", "w", encoding="utf-8") as f:
         json.dump(songs, f, indent=2, ensure_ascii=False)
-    print(f"Generated song_list.json with {len(songs)} entries.")
+    print(f"Generated song_list004.json with {len(songs)} entries.")
